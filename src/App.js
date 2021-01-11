52a07b7a4961ee754b7306b916df1159a5d9c2db
sarahwong <71740977+Wong-S@users.noreply.github.com>
2021-01-11 01:31:20 -0600

    Add time display view

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ import * as moment from 'moment';
 // )
 // };
 
+//NOTE: Time at 1:10AM and 12:52PM yields negative time results!!! So i prob need to factor in military time potentially for AM vs PM...
+
 function SetTime() {
 // eslint-disable-next-line
   const [time, setTime ] = React.useState('');
@@ -60,12 +62,95 @@ function SetTime() {
 
       timeDifference.hours();
       console.log(timeDifference.hours()) //display the hours
-      console.log(typeof timeDifference.hours())
+      console.log(typeof timeDifference.hours()) //Data type is a Number!! 
 
       timeDifference.minutes();
       console.log(timeDifference.minutes()); //display the minutes
 
-      
+      //Now start the count down passing in the hours and minutes
+
+      //First put the hours and minutes display on the frontend html
+      const inputTime = document.querySelector('#countdown').textContent = timeDifference.hours() + ":" + timeDifference.minutes()
+
+      //Then get the time that we just put in the html
+      const timeCount = document.getElementById('countdown');
+
+
+      // countdown code starts here
+      function updateCountDown () {
+        const currentTimeNow = moment(); 
+        const formatTimeHoursMinutes = currentTimeNow.format('hh:mm');
+
+        timeCount.textContent = formatTimeHoursMinutes;
+      }
+
+      setInterval(updateCountDown, 1000);
+
+
+
+
+
+
+
+      // let duration = moment.duration({
+      //   //'days': 1,
+      //   'hours': timeDifference.hours(), //0, //res.hours,
+      //   'minutes': timeDifference.minutes(),
+      //   // 'seconds': 00
+
+      // });
+
+      // let timestamp = new Date(0, 0, 0, 0, 0, 0); //hr, min, sec for last three numbers.
+      // let interval = 1; //dictates speed of the countdown. At 2, it'll countdown faster
+      // let timer = setInterval(function() {
+      //   timestamp = new Date(timestamp.getTime() + interval * 1000); //multiply by 1000 to correct for epoch time
+
+      //   duration = moment.duration(duration.asSeconds() - interval, 'seconds'); //so at 59 seconds and interval at 1, it'll count down to 58, 57, etc.
+      //   let hour = duration.hours(); //defining the variable at hours
+      //   let min = duration.minutes();
+      //   let sec = duration.seconds();
+
+      //   console.log(duration)
+
+      //   //This is where the countdown is executed
+        
+      //   sec -= 1; 
+      //   if (min < 0) return clearInterval(timer);
+      //   if (min < 10 && min.length != 2) min = '0' + min;
+      //   //Nested if Statement below
+      //   if (sec < 0 && min != 0 && hour == 0) {
+      //     //hour -= 1;
+      //     //min -= 1;
+      //     sec = 59;
+      //     if (min != 0){
+      //     min -= 1;
+      //     } 
+      //     if (hour != 0) {
+      //     hour -= 1;
+      //     }
+      //   } else if (sec < 10 && sec.length != 2) sec = '0' + sec;
+
+      //   //USE VANILLA JS, instead of Jquery below
+
+      //   let x = document.querySelector('.countdown').textContent = "hour + ':' + min";
+      //   console.log("hi")
+        
+        
+
+      //   // $('.countdown').text(hour + ':' + min + ':' + sec);
+      //   if (hour == 0 && min == 0 && sec == 0)
+      //     clearInterval(timer);
+      //     // $.get("/countdown-timer-message-twilio"), (res) => {
+      //     //   console.log(res)
+      //     // };
+          
+      //     //$('#alarm-alarm').prepend('href="/countdown-text/{{alarm}}"');
+      //     //document.getElementById("text-send").submit();
+      //     //return "ALARM";
+
+
+      // }, 1000);
+
     }
     countDown()
     
@@ -87,6 +172,8 @@ function SetTime() {
             
             <button>Submit</button>
         </form>
+
+        <div id = "countdown"></div>
     </div>
 )};
 
