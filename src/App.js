81131b997dc10425640dd6ff7e12e3b9486d132d
Ashley Kwan <ashleykwan888@gmail.com>
2021-01-13 07:50:43 -0800

    addeding images folder and icon image

--- src/App.js
+++ src/App.js
@@ -223,18 +223,22 @@ function AddTodo() {
 function App() {
   return (
     <div> 
+
+      <div class="checklistlogo"></div>
+      <div class="icon">Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       <br></br>
       <Search />
       <br></br>
       <AddTodo />
 
-
     </div>
+    
 
 
 
 
   );
 }
+
 
 export default App;
